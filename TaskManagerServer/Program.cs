c71cf0c9855fa69e5422cba8a5d338bdb7836f4a
Prototype150 <prototype248@gmail.com>
2024-03-15 18:26:01 +0100

    Server adress now specified in the args

--- TaskManagerServer/Program.cs
+++ TaskManagerServer/Program.cs
@@ -5,6 +5,8 @@ using TaskManagerServer.DAL.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.WebHost.UseUrls($"https://{args[0]}:{args[1]}", $"http://{args[0]}:{args[2]}");
+
 builder.Services.AddSingleton<IAccountDataManager, AccountMockDataManager>();
 builder.Services.AddSingleton<IAccountManager, AccountManager>();
 builder.Services.AddSingleton<ITaskDataManager, TaskMockDataManager>();
@@ -13,8 +15,6 @@ builder.Services.AddSingleton<ITaskManager, TaskManager>();
 builder.Services.AddControllers();
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
 
 app.UseHttpsRedirection();
 
